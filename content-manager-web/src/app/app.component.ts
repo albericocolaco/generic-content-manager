cffc462ff99740050e192ccb45a0342537d8aa62
Albérico Silva <alberico.silva@compasso.com.br>
2018-06-26 14:43:25 -0300

    Ajuste no layout

--- content-manager-web/src/app/app.component.ts
+++ content-manager-web/src/app/app.component.ts
@@ -8,6 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   content: string;
+  linkStyle: string[];
+
+  eventSetActive(position){
+    let count = 0;
+    this.linkStyle.forEach(element => {
+      if(count === position){
+        element = 'active';
+      } else {
+        element = '';
+      }
+      count++;
+    });
+    
+  }
 
   onContentChanged(event){
     this.content = event.html;
